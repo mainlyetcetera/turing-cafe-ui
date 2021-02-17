9016326bde80b777c5abdd9f99cd70f0a12de0f9
mainlyetcetera <ericcampbell294@gmail.com>
2021-02-17 10:05:46 -0700

    Pass fetched reservation data to Reservations

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,22 @@ import Reservations from '../Reservations/Reservations.js'
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {reservations: []}
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3001/api/v1/reservations')
+    .then(response => response.json())
+    .then(data => this.setState({ 
+      reservations: data
+    }))
+    .catch(err => console.log(err))
+  }
+
   render() {
+    console.log(this.state.reservations)
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
@@ -11,7 +26,7 @@ class App extends Component {
 
         </div>
         <div className='resy-container'>
-          <Reservations />
+          <Reservations reservations={this.state.reservations}/>
         </div>
       </div>
     )
--- src/components/Reservations/Reservations.js
+++ src/components/Reservations/Reservations.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import Reservation from '../Reservation/Reservation.js'
 
-const Reservations = data => {
-  return data.map(piece => <Reservation />)
+const Reservations = ({reservations}) => {
+  console.log('reservations', reservations)
+  return reservations.map(reservation => <Reservation />)
 }
 
 export default Reservations
