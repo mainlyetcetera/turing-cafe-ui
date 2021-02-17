ac55ab80889c91176c1cc7d896b2d825711da687
mainlyetcetera <ericcampbell294@gmail.com>
2021-02-17 11:42:22 -0700

    Set up skeleton for initial-view tests

---
+++ cypress/integration/initial-visit-spec.js
@@ -0,0 +1,24 @@
+describe('the initial view', () => {
+  it('should visit the page', () => {
+    cy
+      .fixture('../fixtures/testData.json')
+      .then(data => {
+        cy.intercept('GET', 'http://localhost:3001/api/v1/reservations', {
+          statusCode: 200,
+          body: data.data
+        })
+      })
+
+    cy
+      .visit('localhost:3000')
+  })
+
+  it('should have a title', () => {
+  })
+
+  it('should have a form', () => {
+  })
+
+  it('should start with reservations', () => {
+  })
+})
