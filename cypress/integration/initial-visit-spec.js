2fd21260439e0291313dbef3eb86eaeb9e20d9ca
mainlyetcetera <ericcampbell294@gmail.com>
2021-02-17 12:06:21 -0700

    Test typing inputs

--- cypress/integration/initial-visit-spec.js
+++ cypress/integration/initial-visit-spec.js
@@ -19,9 +19,62 @@ describe('the initial view', () => {
       .contains('Turing Cafe Reservations')
   })
 
-  it('should have a form', () => {
+  it('should have a name input', () => {
+    cy
+      .get('body')
+      .contains('Name')
+  })
+
+  it('should have a date input', () => {
+    cy
+      .get('body')
+      .contains('Date')
+  })
+  
+  it('should have a time input', () => {
+    cy
+      .get('body')
+      .contains('Time')
+  })
+
+  it('should have a number input', () => {
+    cy
+      .get('body')
+      .contains('Number')
   })
 
   it('should start with reservations', () => {
+    cy
+      .get('section')
+      .children()
+      .should('have.length', 2)
+
+      .get('section')
+      .children('article:first')
+      .contains('Christie')
+  })
+})
+
+describe('the form', () => {
+  it('should visit the page', () => {
+    cy
+      .fixture('../fixtures/testData.json')
+      .then(data => {
+        cy.intercept('GET', 'http://localhost:3001/api/v1/reservations', {
+          statusCode: 200,
+          body: data.data
+        })
+      })
+
+    cy
+      .visit('localhost:3000')
+  })
+
+  it('should be able to contain characters', () => { 
+    cy
+      .get('form div')
+      .children('input:first')
+      .type('asdf')
+      .should('have.value', 'asdf')
   })
 })
