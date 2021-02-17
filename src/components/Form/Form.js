131a4f1ccf0fc13371a6e8c0f1bf520005ce947e
mainlyetcetera <ericcampbell294@gmail.com>
2021-02-17 10:55:28 -0700

    Add other form fields

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -17,13 +17,39 @@ export default class Form extends Component {
 
   render() {
     return (
-      <input
-        type='text'
-        name='name'
-        placeholder='Name'
-        value={this.state.name}
-        onChange={event => this.handleChange(event)}
-      />
+      <div>
+        <input
+          type='text'
+          name='name'
+          placeholder='Name'
+          value={this.state.name}
+          onChange={event => this.handleChange(event)}
+        />
+
+        <input
+          type='text'
+          name='date'
+          placeholder='Date'
+          value={this.state.date}
+          onChange={event => this.handleChange(event)}
+        />
+
+        <input
+          type='text'
+          name='time'
+          placeholder='Time'
+          value={this.state.time}
+          onChange={event => this.handleChange(event)}
+        />
+
+        <input
+          type='text'
+          name='number'
+          placeholder='Number'
+          value={this.state.number}
+          onChange={event => this.handleChange(event)}
+        />
+      </div>
     )
   }
 }
